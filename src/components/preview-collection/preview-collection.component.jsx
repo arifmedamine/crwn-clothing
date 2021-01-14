96a803b251907c40e1d5d2b5a81c579f1a3f5455
arifmedamine <mohamedamine.arif@esprit.tn>
2021-01-14 18:09:32 +0100

    adding item to cart with redux

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -6,8 +6,10 @@ const PreviewCollection = ({title, items}) => (
     <div className='collection-preview'>
         <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {items.filter((item, idx) => idx < 4).map(({id, ...otherItemProps}) => (
-                <CollectionItem key={items.id} {...otherItemProps}/>
+            {items
+                .filter((item, idx) => idx < 4)
+                .map((item) => (
+                <CollectionItem key={item.id} item={item}/>
             ))}
         </div>
     </div>
